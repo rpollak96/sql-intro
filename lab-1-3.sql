b6843ea9a2b4c11e9fb2fa1e8c3113d1313f0009
rpollak96 <rachael.pollak@kellogg.northwestern.edu>
2026-01-15 01:49:30 +0000

    seperated code in lab-1-3 by line

--- lab-1-3.sql
+++ lab-1-3.sql
@@ -7,5 +7,9 @@
 -- | 1962 | New York Mets.       | 40   | 120    |
 -- | 2003 | Detroit Tigers       | 43   | 119    |
 -- +------+----------------------+------+--------+
-SELECT year, name, wins, losses FROM teams ORDER BY losses DESC LIMIT 3;
+SELECT year, name, wins, losses 
+FROM teams 
+ORDER BY losses 
+DESC 
+LIMIT 3;
 
