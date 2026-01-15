5193dfab9544e34c684ee44fe444c6ff3ebd4cba
rpollak96 <rachael.pollak@kellogg.northwestern.edu>
2026-01-15 01:46:12 +0000

    lab-1-3 answered

--- lab-1-3.sql
+++ lab-1-3.sql
@@ -7,5 +7,5 @@
 -- | 1962 | New York Mets.       | 40   | 120    |
 -- | 2003 | Detroit Tigers       | 43   | 119    |
 -- +------+----------------------+------+--------+
-
+SELECT year, name, wins, losses FROM teams ORDER BY losses DESC LIMIT 3;
 
