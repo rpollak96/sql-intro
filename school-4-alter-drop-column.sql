af07bf7bbc6a9b43ac63b4884fc064917b02e12a
rpollak96 <rachael.pollak@kellogg.northwestern.edu>
2026-01-15 03:08:41 +0000

    school 1-4 done

--- school-4-alter-drop-column.sql
+++ school-4-alter-drop-column.sql
@@ -1,2 +1,7 @@
 -- Drop the phone number column
 
+ALTER TABLE students DROP COLUMN phone_number; -- TEXT
+
+-- make notes to help you easily re-add column later on if needed (i.e. added TEXT comment)
+-- future proof code/leaves notes to help fix mistakes 
+-- lots of alterations can be made, look them up as needed
